7b5f5c293fd60c3fcc9702e7aab2bee3da5ae863
Aminul667 <rahathssn@gmail.com>
2026-02-07 13:19:27 +0600

    added basic setup

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from "express";
+
+
+const app: Application = express()
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Vigor server is running");
+});
+
+export default app;
