c284b03a661dd2fd3f730579eb16542d33fbde27
Aminul667 <rahathssn@gmail.com>
2026-02-08 18:57:46 +0600

    added global error handler and not found middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import { IndexRoutes } from "./app/routes";
+import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
 
 const app: Application = express();
 
@@ -12,5 +14,8 @@ app.use("/api/v1", IndexRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send("Vigor server is running");
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
