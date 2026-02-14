ac14060272ffaf6b8868255de714f29c6507f9c4
Aminul667 <rahathssn@gmail.com>
2026-02-14 19:56:50 +0600

    added get all doctor functionality

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { SpecialityRoutes } from "../module/speciality/speciality.route";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { UserRoutes } from "../module/user/user.route";
+import { DoctorRoutes } from "../module/doctor/doctor.route";
 
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialityRoutes);
 router.use("/users", UserRoutes);
+router.use("/doctors", DoctorRoutes);
 
 export const IndexRoutes = router;
